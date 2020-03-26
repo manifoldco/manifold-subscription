8bb2899e57d09cbcd944c81d384d02da6fd2d721
David Leger <dleger@manifold.co>
2020-03-26 18:09:14 -0300

    bunch of stuff

---
+++ src/components/manifold-subscription-create/components/Message.tsx
@@ -0,0 +1,15 @@
+import { h, FunctionalComponent } from '@stencil/core';
+interface MessageProps {
+  type?: 'success' | 'error';
+}
+
+const resolveClass = (type: MessageProps['type']) =>
+  `ManifoldSubscriptionCreate__Message ${
+    type ? `ManifoldSubscriptionCreate__Message--${type}` : ''
+  }`;
+
+const Message: FunctionalComponent<MessageProps> = ({ type }, children) => (
+  <p class={resolveClass(type)}>{children}</p>
+);
+
+export default Message;
--- src/components/manifold-subscription-create/components/PlanCard.tsx
+++ src/components/manifold-subscription-create/components/PlanCard.tsx
@@ -2,13 +2,23 @@ import { h, FunctionalComponent } from '@stencil/core';
 import { PlanQuery } from '../../../types/graphql';
 import { $ } from '../../../utils/currency';
 
-type PlanCardProps = PlanQuery['plan'];
+interface PlanCardProps {
+  isLoading?: boolean;
+  plan?: PlanQuery['plan'];
+}
 
-const PlanCard: FunctionalComponent<PlanCardProps> = ({ displayName, cost }) => (
+const defaultPlan: PlanCardProps['plan'] = {
+  displayName: 'Plan Name',
+  cost: 0,
+};
+
+const PlanCard: FunctionalComponent<PlanCardProps> = ({ isLoading, plan = defaultPlan }) => (
   <div class="ManifoldSubscriptionCreate__PlanCard">
-    <div class="ManifoldSubscriptionCreate__PlanCard__Name">{displayName}</div>
-    <span class="ManifoldSubscriptionCreate__PlanCard__Cost">
-      {$(cost)}
+    <div class="ManifoldSubscriptionCreate__PlanCard__Name" data-is-loading={isLoading}>
+      {plan.displayName}
+    </div>
+    <span class="ManifoldSubscriptionCreate__PlanCard__Cost" data-is-loading={isLoading}>
+      {$(plan.cost)}
       <span class="ManifoldSubscriptionCreate__PlanCard__Cost__Suffix">/mo</span>
     </span>
   </div>
