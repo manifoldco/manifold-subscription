af3227087e032f6e9e199bcf282cf5bffeee6618
David Leger <dleger@manifold.co>
2020-03-30 10:38:13 -0300

    add all the workflows and buildy stuff

---
+++ .happo.js
@@ -0,0 +1,18 @@
+const { RemoteBrowserTarget } = require('happo.io');
+const happoPluginTypeScript = require('happo-plugin-typescript');
+const path = require('path');
+
+module.exports = {
+  project: 'manifoldco/manifold-subscription',
+  apiKey: process.env.HAPPO_API_KEY,
+  apiSecret: process.env.HAPPO_API_SECRET,
+  targets: {
+    chrome: new RemoteBrowserTarget('chrome', {
+      viewport: '1440x900',
+    }),
+  },
+  type: 'plain',
+  prerender: false,
+  setupScript: path.resolve(__dirname, 'happo.setup.js'),
+  plugins: [happoPluginTypeScript()],
+};
