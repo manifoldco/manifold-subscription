e956bf01acbba95ca5cf07a1e9441c73a2d29a1c
David Leger <dleger@manifold.co>
2020-04-22 16:10:25 -0300

    use mercury for fields

--- src/components/shared/Select.tsx
+++ src/components/shared/Select.tsx
@@ -18,17 +18,15 @@ const Select: FunctionalComponent<SelectProps> = props => {
   const { options, selectedValue, onChange, ...rest } = props;
 
   return (
-    <select
-      class="ManifoldSubscriptionCreate__Select"
-      onChange={e => onChange(rest.name, (e.target as HTMLInputElement).value)}
-      {...rest}
-    >
-      {options.map(({ value, label }) => (
-        <option value={value} selected={value === selectedValue}>
-          {label}
-        </option>
-      ))}
-    </select>
+    <div class="ManifoldSubscriptionCreate__Select">
+      <select onChange={e => onChange(rest.name, (e.target as HTMLInputElement).value)} {...rest}>
+        {options.map(({ value, label }) => (
+          <option value={value} selected={value === selectedValue}>
+            {label}
+          </option>
+        ))}
+      </select>
+    </div>
   );
 };
 
